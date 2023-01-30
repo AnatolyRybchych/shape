bbc18809a9390154c07711abe823849e52c17f32
Anatoly Rybchych <tol.ryb@gmail.com>
2023-01-30 21:36:56 +0200

    exit with error on panic

--- src/panic.h
+++ src/panic.h
@@ -7,12 +7,14 @@
 #define PANIC_LTZ(EXPRESSION) do{\
     if(EXPRESSION < 0){\
         fprintf(stderr, "PANIC in %s:%i %s\n%s < 0\n\n", __FILE__, __LINE__, __func__, __STRING(EXPRESSION));\
+        exit(-1);\
     }\
 } while(0)
 
 #define PANIC_NULL(EXPRESSION) do{\
     if(EXPRESSION == NULL){\
         fprintf(stderr, "PANIC in %s:%i %s\n%s == NULL\n\n", __FILE__, __LINE__, __func__, __STRING(EXPRESSION));\
+        exit(-1);\
     }\
 } while(0)
 
